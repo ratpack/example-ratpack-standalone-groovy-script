42b7292d8cfb927b94a9653766928a2a490fbd99
Luke Daley <ld@ldaley.com>
2013-05-14 10:36:40 +0100

    Formatting.

--- ratpack.groovy
+++ ratpack.groovy
@@ -1,20 +1,19 @@
-@GrabResolver(name = "Sonatype OSS Snapshots", root = "https://oss.sonatype.org/content/repositories/snapshots", m2Compatible = true)
-@Grab(value = "org.ratpack-framework:ratpack-groovy:0.7.0-SNAPSHOT", changing = true)
+@GrabResolver(name = "Sonatype OSS Snapshots", root = "https://oss.sonatype.org/content/repositories/snapshots", m2Compatible = true) @Grab(value = "org.ratpack-framework:ratpack-groovy:0.7.0-SNAPSHOT", changing = true)
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 import org.ratpackframework.groovy.templating.TemplateRenderer
 
 // You can change anything in the ratpack {} closure without needing to restart
 
 ratpack {
-   routing {
-       get {
-           response.send "This is the app root (also try: /date and /some.txt)"
-       }
+    routing {
+        get {
+            response.send "This is the app root (also try: /date and /some.txt)"
+        }
 
-       get("date") {
-           get(TemplateRenderer).render "date.html"
-       }
+        get("date") {
+            get(TemplateRenderer).render "date.html"
+        }
 
-	   assets "public"
-   }
+        assets "public"
+    }
 }
