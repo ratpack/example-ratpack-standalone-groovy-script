66360421992dac9028212068cb9640a182011c01
Luke Daley <ld@ldaley.com>
2013-05-17 13:16:35 +0100

    Update Ratpack.

--- ratpack.groovy
+++ ratpack.groovy
@@ -1,12 +1,12 @@
 @GrabResolver(name = "Sonatype OSS Snapshots", root = "https://oss.sonatype.org/content/repositories/snapshots", m2Compatible = true)
-@Grab(value = "org.ratpack-framework:ratpack-groovy:0.7.0-SNAPSHOT", changing = true)
+@Grab(value = "org.ratpack-framework:ratpack-groovy:0.9.0-SNAPSHOT", changing = true)
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 import org.ratpackframework.groovy.templating.TemplateRenderer
 
 // You can change anything in the ratpack {} closure without needing to restart
 
 ratpack {
-    routing {
+    handlers {
         get {
             response.send "This is the app root (also try: /date and /some.txt)"
         }
