b4cd9bb0d8caad6fc433ffdcce4f1c14ec4a784c
Luke Daley <ld@ldaley.com>
2013-08-09 23:03:20 +0100

    update.

--- ratpack.groovy
+++ ratpack.groovy
@@ -1,7 +1,7 @@
 @GrabResolver("https://oss.jfrog.org/artifactory/repo")
 @Grab("org.ratpack-framework:ratpack-groovy:0.9.0-SNAPSHOT")
 import static org.ratpackframework.groovy.RatpackScript.ratpack
-import org.ratpackframework.groovy.templating.TemplateRenderer
+import static org.ratpackframework.groovy.Template.groovyTemplate
 
 // You can change anything in the ratpack {} closure without needing to restart
 
@@ -11,8 +11,8 @@ ratpack {
             response.send "This is the app root (also try: /date and /some.txt)"
         }
 
-        get("date") { TemplateRenderer renderer ->
-            renderer.render "date.html"
+        get("date") {
+            render groovyTemplate("date.html")
         }
 
         assets "public"
