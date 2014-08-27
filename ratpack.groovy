9adbb984e4bd1d713dbc70c179caad4eea238c21
rhart <russellxhart@googlemail.com>
2014-08-27 12:37:36 +0100

    Update to 0.9.7

--- ratpack.groovy
+++ ratpack.groovy
@@ -1,6 +1,7 @@
 @GrabResolver("https://oss.jfrog.org/artifactory/repo")
-@Grab("io.ratpack:ratpack-groovy:0.9.0-SNAPSHOT")
-import static ratpack.groovy.Groovy.*
+@Grab("io.ratpack:ratpack-groovy:0.9.7")
+import static ratpack.groovy.Groovy.ratpack
+import static ratpack.groovy.Groovy.groovyTemplate
 
 // You can change anything in the ratpack {} closure without needing to restart
 
