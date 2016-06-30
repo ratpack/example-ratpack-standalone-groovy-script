2fa200b3777292924d319955ad0e8f9b6c49a4ba
Danny Hyun <hyun.danny+github@gmail.com>
2016-06-30 15:43:51 -0400

    Upgrade to Ratpack 1.3.3

--- ratpack.groovy
+++ ratpack.groovy
@@ -1,5 +1,4 @@
-@GrabResolver(name="OJO", root="https://oss.jfrog.org/artifactory/repo")
-@Grab("io.ratpack:ratpack-groovy:0.9.18")
+@Grab("io.ratpack:ratpack-groovy:1.3.3")
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import ratpack.groovy.template.MarkupTemplateModule
